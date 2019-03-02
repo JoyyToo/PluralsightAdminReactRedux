9cc4ac3b9a2042b755f0dd1e6fbf1ce95f78cb4c
joyytoo <joytoo97@gmail.com>
2019-03-02 04:16:06 +0300

    Integrate redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,4 @@
 import React, { PropTypes } from "react";
-import { Route, Switch } from "react-router-dom";
-import HomePage from "./home/HomePage";
-import AboutPage from "./about/AboutPage";
 import Header from "./common/header";
 
 class App extends React.Component {
